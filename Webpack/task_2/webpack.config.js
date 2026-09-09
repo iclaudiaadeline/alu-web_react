790b146c37c7a5d3f61abcccce5d7c1d6f1fcf68
iclaudiaadeline <a.iradukund3@alustudent.com>
2026-09-09 19:22:29 +0200

    Cannot generate commit message: no workspace

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,24 +1,18 @@
+```js
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
+
   entry: './js/dashboard_main.js',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    clean: true,
   },
+
   devtool: 'inline-source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'public'),
-    },
-    contentBase: './public',
-    port: 8564,
-    open: true,
-  },
+
   module: {
     rules: [
       {
@@ -41,13 +35,9 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Holberton Dashboard',
-    }),
-  ],
+
   performance: {
     hints: false,
   },
 };
+```
