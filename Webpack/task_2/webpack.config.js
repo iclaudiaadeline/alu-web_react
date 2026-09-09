49e3eff164801780f42959196f872f7477c7a515
iclaudiaadeline <a.iradukund3@alustudent.com>
2026-09-09 19:26:34 +0200

    updated

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,43 +1,34 @@
-```js
 const path = require('path');
 
 module.exports = {
   mode: 'production',
-
-  entry: './js/dashboard_main.js',
-
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  performance: {
+    maxAssetSize: 1000000,
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-
-  devtool: 'inline-source-map',
-
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        type: 'asset/resource',
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
+          "file-loader",
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
-              mozjpeg: {
-                progressive: true,
-              },
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
       },
     ],
   },
-
-  performance: {
-    hints: false,
-  },
 };
-```
